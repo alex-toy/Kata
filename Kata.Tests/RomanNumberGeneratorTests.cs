01a3665d438447129efae65d8c5b4057b4b00477
alessio <alessio.rea@keyconsulting.fr>
2022-07-07 14:09:15 +0200

    Algorithm works for 7 first integers.

---
+++ Kata.Tests/RomanNumberGeneratorTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+
+namespace Kata.Tests
+{
+    [TestFixture]
+    class RomanNumberGeneratorTests
+    {
+        [TestCase(1, "I")][TestCase(2, "II")][TestCase(3, "III")]
+        [TestCase(4, "IV")][TestCase(5, "V")]
+        [TestCase(6, "VI")][TestCase(7, "VII")]
+        public void ToRoman_Should_Return_Exact_Roman_Number_Corresponding_To_Parameter(int arabNumber, string expectedRomanNumber)
+        {
+            string result = RomanNumberGenerator.ToRoman(arabNumber);
+            result.Should().Be(expectedRomanNumber);
+        }
+    }
+}
---
+++ Kata/RomanNumberGenerator.cs
@@ -0,0 +1,12 @@
+﻿namespace Kata
+{
+    public class RomanNumberGenerator
+    {
+        public static string ToRoman(int arabNumber)
+        {
+            if (arabNumber <= 3) return new string('I', arabNumber);
+            if (arabNumber == 4) return "IV";
+            return "V" + ToRoman(arabNumber-5);
+        }
+    }
+}
